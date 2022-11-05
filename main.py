5690dd3cabf89d0bd1aa536ecac5ad0973d849ad
AshleyRoyce <99229014+AshleyRoyce@users.noreply.github.com>
2022-11-05 10:12:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import requests
 import json
 import pydeck as pdk
+import plotly.express as px
+
 def get_crypto_prices(symbol, start, end):
     api_key = '076c246c09cc513816e38f7cadc0e490'
     series = pd.date_range(start, end)
@@ -83,21 +85,15 @@ if add_selectbox == "Current Cryptocurrency Data":
         st.table(get_crypto_prices(desired_coin, todays_date, end_date))
     elif all_coins:
         st.table(get_all_prices(todays_date, end_date))
-    st.subheader("Current Cryptocurrency Comparisons")
-    options = st.multiselect(
-        'Choose the coins you would like to compare',
-        ['BTC', 'ETH', 'LTC', 'USDT', 'BNB', 'XRP'],
-        ['BTC', 'ETH', 'LTC'])
-    st.write('You selected:', options)
-    priceValue = []
-    for i in range(len(options)):
-        priceValue.append(get_crypto_prices(options[i], todays_date, todays_date))
-
-    chart_data = pd.util.hash_pandas_object(pd.DataFrame({
-        'Coin': [options],
-        'Price': [priceValue]
-    }))
+    st.subheader("Cryptocurrency Comparisons")
+    chart_data = {
+        'Coin': ['LTC', 'BNB', 'XMR', 'XRP', 'AAVE', 'BSV', 'ZEC'],
+        'Price(USD)': [69.75, 353.28, 160.07, 163.18, 95.52, 49.06, 54.17]
+    }
+    chart_data = pd.DataFrame(chart_data)
+    chart_data = chart_data.set_index("Coin")
     st.bar_chart(chart_data)
+    st.text("Prices as of 11/5/2022")
 
 
 elif add_selectbox == "Historical Data":
@@ -111,39 +107,40 @@ elif add_selectbox == "Historical Data":
 elif add_selectbox == "Global Cryptocurrency Conversions":
     st.header("Global Cryptocurrency Conversions")
     st.subheader("Map- Top Countries by Cryptocurrency Ownership")
-    df = pd.read_csv("csv/Crypto_Capitals_2022.csv")
+    if st.button('Click here to view map of top crypto-owning countries'):
+        df = pd.read_csv("csv/Crypto_Capitals_2022.csv")
 
-    zoom_lat = df["latitude"].mean()
-    zoom_long = df["longitude"].mean()
+        zoom_lat = df["latitude"].mean()
+        zoom_long = df["longitude"].mean()
 
-    st.pydeck_chart(pdk.Deck(
-        # map_style https://docs.mapbox.com/api/maps/styles/
-        map_style='mapbox://styles/mapbox/light-v10',
-        initial_view_state=pdk.ViewState(
-            latitude=zoom_lat,
-            longitude=zoom_long,
-            zoom=0.5,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=df,
-                get_position='[longitude, latitude]',
-                get_color='[26, 255, 0, 160]',
-                get_radius=150000,
-                pickable=True,
+        st.pydeck_chart(pdk.Deck(
+            # map_style https://docs.mapbox.com/api/maps/styles/
+            map_style='mapbox://styles/mapbox/light-v10',
+            initial_view_state=pdk.ViewState(
+                latitude=zoom_lat,
+                longitude=zoom_long,
+                zoom=0.5,
+                pitch=50,
             ),
-        ],
-        tooltip={
-            "html": "{name} <br/> % of population owning crypto: {percent}% <br/>",
-            "style": {
-                "backgroundColor": "steelblue",
-                "color": "white"
+            layers=[
+                pdk.Layer(
+                    'ScatterplotLayer',
+                    data=df,
+                    get_position='[longitude, latitude]',
+                    get_color='[26, 255, 0, 160]',
+                    get_radius=150000,
+                    pickable=True,
+                ),
+            ],
+            tooltip={
+                "html": "{name} <br/> % of population owning crypto: {percent}% <br/>",
+                "style": {
+                    "backgroundColor": "steelblue",
+                    "color": "white"
+                }
             }
-        }
-    ))
-    st.info('Statistical information displayed in map collected in 2021 by TripleA', icon="ℹ️")
+        ))
+        st.info('Statistical information displayed in map collected in 2021 by TripleA', icon="ℹ️")
 
     st.subheader("Cryptocurrency Converter by Country")
     coin = st.radio("Choose a Cryptopcurrency",
@@ -167,13 +164,70 @@ elif add_selectbox == "Global Cryptocurrency Conversions":
         btc_price = response["USD"]
         st.write("Current price of Litecoin in US$ {}".format(btc_price))
 
-    if st.button('Click here to perform currency conversions'):
-        capital = st.selectbox('Select a country to convert cryptocurrency prices',
+    capital = st.selectbox('Select a country to convert cryptocurrency prices',
                                ["Brazil", "Colombia", "India", "Indonesia", "Kenya", "Nigeria",
-                                "Pakistan", "Phillipines", "Russia", "South Africa", "Thailand",
-                                "Ukraine", "United Kingdom", "United States", "Venezuela", "Vietnam"])
+                                "Pakistan", "Philippines", "Russia", "South Africa", "Thailand",
+                                "Ukraine", "United Kingdom", "Venezuela", "Vietnam"])
 
+    if coin and capital == "Brazil":
+        coin_price = (btc_price * 5.05)
+        st.write("Current price of " + coin + " in BRL is: " + str(round(coin_price, 2)))
 
+    if coin and capital == "Colombia":
+        coin_price = (btc_price * 4974.21)
+        st.write("Current price of " + coin + " in COP is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "India":
+        coin_price = (btc_price * 81.98)
+        st.write("Current price of " + coin + " in INR is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Indonesia":
+        coin_price = (btc_price * 15614.60)
+        st.write("Current price of " + coin + " in IDR is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Kenya":
+        coin_price = (btc_price * 119.23)
+        st.write("Current price of " + coin + " in KES is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Nigeria":
+        coin_price = (btc_price * 439.38)
+        st.write("Current price of " + coin + " in NGN is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Pakistan":
+        coin_price = (btc_price * 217.86)
+        st.write("Current price of " + coin + " in PKR is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Philippines":
+        coin_price = (btc_price * 58.33)
+        st.write("Current price of " + coin + " in PHP is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Russia":
+        coin_price = (btc_price * 62.00)
+        st.write("Current price of " + coin + " in RUB is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "South Africa":
+        coin_price = (btc_price * 17.90)
+        st.write("Current price of " + coin + " in ZAR is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Thailand":
+        coin_price = (btc_price * 37.29)
+        st.write("Current price of " + coin + " in THB is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Ukraine":
+        coin_price = (btc_price * 36.06)
+        st.write("Current price of " + coin + " in UAH is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "United Kingdom":
+        coin_price = (btc_price * 0.88)
+        st.write("Current price of " + coin + " in GBP is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Venezuela":
+        coin_price = (btc_price * 8.63545)
+        st.write("Current price of " + coin + " in VES is: " + str(round(coin_price, 2)))
+
+    if coin and capital == "Vietnam":
+        coin_price = (btc_price * 24873.50)
+        st.write("Current price of " + coin + " in VND is: " + str(round(coin_price, 2)))
 
 else:
     st.header("CAP 4104 - Developed by Adriel Molerio & Ashley Royce")
